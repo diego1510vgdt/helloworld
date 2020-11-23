9a675bc4dc56723ea4a6118a2b9be955a6d22143
Diego Vega De Ita <diegovdi@pop-os.localdomain>
2020-11-23 15:29:36 -0600

    Se agregaron las tablas a la base de datos y se realizaron algunas inserciones dentro de las tablas. Se comunicaron entre ellas.

--- helloworld_project/pages/admin.py
+++ helloworld_project/pages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Autor, Publication, Comments
 
 # Register your models here.
+
+admin.site.register(Autor)
+admin.site.register(Publication)
+admin.site.register(Comments)
--- helloworld_project/pages/models.py
+++ helloworld_project/pages/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+#Modelo del autor
+class Autor(models.Model):
+    nombre = models.CharField(max_length = 30)
+    
+    def __str__(self):
+        return self.nombre
+
+#Modelo de publicaciones
+class Publication(models.Model):
+    titulo = models.CharField(max_length = 50)
+    contenido = models.CharField(max_length = 250)
+    fecha_creacion = models.DateField()
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+#Modelo de comentarios
+class Comments(models.Model):
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    fecha_creacion = models.DateField()
+    contenido = models.CharField(max_length = 200)
+    idPub = models.ForeignKey(Publication, on_delete=models.CASCADE)
+    reccion = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.contenido
